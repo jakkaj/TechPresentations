9c7065331353eea8c6baa74e6c8dc3e5cb7ce7d2
Jordan Knight <jakkaj@gmail.com>
2014-10-03 12:57:24 +1000

    Added TestController Set startup path

---
+++ TechEd/2014/Lets Talk Auth/WebAPI Demo/WebAPI Auth Demo/Teched2014_WebAPI_Auth_Demo/Controllers/api/TestController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Teched2014_WebAPI_Auth_Demo.Controllers.api
+{
+    public class TestController : ApiController
+    {
+        [Authorize]
+        public async Task<IHttpActionResult> Get()
+        {
+            return Ok("Test");
+        }
+    }
+}
