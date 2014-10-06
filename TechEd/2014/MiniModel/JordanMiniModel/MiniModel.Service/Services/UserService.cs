d9846b0b6b76c346cf3da3876684b68a40a0a1b9
Jordan Knight <jakkaj@gmail.com>
2014-10-06 15:21:07 +1100

    users can now be saved

--- TechEd/2014/MiniModel/JordanMiniModel/MiniModel.Service/Services/UserService.cs
+++ TechEd/2014/MiniModel/JordanMiniModel/MiniModel.Service/Services/UserService.cs
@@ -10,15 +10,16 @@ namespace MiniModel.Service.Services
 {
     public class UserService : IUserService
     {
+        private static User _user = null;
+        public async Task SetCurrentUser(User u)
+        {
+            _user = u;
+        }
         public async Task<User> GetCurrentUser()
         {
-            await Task.Delay(2000);
+            await Task.Delay(1000);
 
-            return new User()
-            {
-                Handle = "Jakkaj",
-                Id = Guid.NewGuid()
-            };
+            return _user;
         }
     }
 }
