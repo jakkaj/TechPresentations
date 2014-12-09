285aeda55a00bfd5d93876e635a75de10ded31ac
Jordan Knight <jakkaj@gmail.com>
2014-12-09 11:14:10 +1100

    Base Demo

---
+++ Xamarin Bits/MobileApp/MobileApp.iOS/BasicForm/BasePage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MobileApp.iOS.BasicForm
+{
+    public class BasePage : ContentPage
+    {
+        public BasePage()
+        {
+            
+        }
+    }
+}
---
+++ Xamarin Bits/MobileApp/MobileApp.iOS/Step 1/iOSAlert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using MonoTouch.UIKit;
+
+namespace MobileApp.iOS.Step_1
+{
+    public class iOSAlert
+    {
+        public void DisplayAlert(string title, string message)
+        {
+            var alert = new UIAlertView(title, message, null, "Cancel",
+                                   "Ok");
+            alert.Clicked += (sender, args) => Debug.WriteLine(args.ButtonIndex);
+            alert.Show();
+        }
+    }
+}
