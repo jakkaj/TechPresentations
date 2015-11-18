0557155f2c4489ca95e9e428d24946df54bfedc0
Jordan Knight <jakkaj@gmail.com>
2015-11-18 20:01:24 +1100

    Added missing file

---
+++ Ignite/2015/YouCore/YouCore.API/Controllers/API/Notification/NotifyController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using YouCore.NET.Service;
+using YouCore.Web.Support.Contract;
+
+namespace YouCore.API.Controllers.API.Notification
+{
+    [RoutePrefix("api/notify")]
+    public class NotifyController : ApiController
+    {
+        private readonly INotificationService _notificationService;
+        private readonly IOperationResponder _response;
+
+        public NotifyController(INotificationService notificationService, IOperationResponder response)
+        {
+            _notificationService = notificationService;
+            _response = response;
+        }
+
+        [Route("")]
+        [HttpGet]
+        public async Task<IHttpActionResult> Notify(string title, string message)
+        {
+            var result = await _notificationService.SendWindowsNative(message, title);
+            return _response.ActionResult(result, Request);
+        }
+    }
+}
