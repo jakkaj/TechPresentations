5aed91a125d71194da1d9c74d6f9aabbd3838427
Jordan Knight <jakkaj@gmail.com>
2016-04-28 23:36:19 +1000

    Added console version

---
+++ CloudRoadshow/RoadConsole/RoadConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RoadConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = args[0];
+            _get(url);
+
+            Thread.Sleep(20000);
+
+        }
+
+        static async void _get(string url)
+        {
+            Console.WriteLine(await MarkdownGetter.GetMarkdown(url));
+        }
+    }
+}
