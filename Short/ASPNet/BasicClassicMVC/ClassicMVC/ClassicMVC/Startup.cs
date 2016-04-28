c9fd668c15a559b19ca9ad892dfd59b9d5674f17
Jordan Knight <jakkaj@gmail.com>
2016-04-28 11:19:04 +1000

    Added classic mvc template

---
+++ Short/ASPNet/BasicClassicMVC/ClassicMVC/ClassicMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClassicMVC.Startup))]
+namespace ClassicMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
