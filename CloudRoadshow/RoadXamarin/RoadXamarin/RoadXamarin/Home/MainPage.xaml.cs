6e3a009c9403925fd992ee6c0c5ba0dfa1297402
Jordan Knight <jakkaj@gmail.com>
2016-04-29 01:08:34 +1000

    Added Xamarin version

---
+++ CloudRoadshow/RoadXamarin/RoadXamarin/RoadXamarin/Home/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace RoadXamarin.Home
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var result = await MarkdownGetter.GetMarkdown(ThisUrl.Text);
+
+            var modelResult = JsonConvert.DeserializeObject<UrlModel>(result);
+
+            MyBrowser.Source = new HtmlWebViewSource {Html = modelResult.Result};
+        }
+    }
+}
