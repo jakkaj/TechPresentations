69d83a9b45c5c920d3151bb0721a6d91045469f8
Jordan Knight <jakkaj@outlook.com>
2016-06-08 10:41:35 +1000

    Added a basic demo for surface hub stuff.

---
+++ Short/SurfaceHub/ColorsAndPens/ColorsAndPensDemo/ColorUtils.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorsAndPensDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Windows.UI;
+
+    namespace TimeToShineClient.Util
+    {
+        public static class ColorUtils
+        {
+            /// <summary>
+            /// Convert rgb to hsv color
+            /// </summary>
+            /// <param name="color">rgb color</param>
+            /// <returns>hsv color</returns>
+            public static float[] ToHSV(Color color)
+            {
+                var rgb = new float[]
+                {
+                color.R / 255f, color.G / 255f, color.B / 255f
+                };
+
+                // RGB to HSV
+                float max = rgb.Max();
+                float min = rgb.Min();
+
+                float h, s, v;
+                if (max == min)
+                {
+                    h = 0f;
+                }
+                else if (max == rgb[0])
+                {
+                    h = (60f * (rgb[1] - rgb[2]) / (max - min) + 360f) % 360f;
+                }
+                else if (max == rgb[1])
+                {
+                    h = 60f * (rgb[2] - rgb[0]) / (max - min) + 120f;
+                }
+                else
+                {
+                    h = 60f * (rgb[0] - rgb[1]) / (max - min) + 240f;
+                }
+
+                if (max == 0d)
+                {
+                    s = 0f;
+                }
+                else
+                {
+                    s = (max - min) / max;
+                }
+                v = max;
+
+                return new float[] { h, s, v };
+            }
+
+            /// <summary>
+            /// Convert hsv to rgb color
+            /// </summary>
+            /// <param name="hue">hue</param>
+            /// <param name="saturation">saturation</param>
+            /// <param name="brightness">brightness</param>
+            /// <returns>Color</returns>
+            public static Color FromHsv(float hue, float saturation, float brightness)
+            {
+                if (saturation == 0)
+                {
+                    var c = (byte)Math.Round(brightness * 255f, MidpointRounding.AwayFromZero);
+                    return ColorHelper.FromArgb(0xff, c, c, c);
+                }
+
+                var hi = ((int)(hue / 60f)) % 6;
+                var f = hue / 60f - (int)(hue / 60d);
+                var p = brightness * (1 - saturation);
+                var q = brightness * (1 - f * saturation);
+                var t = brightness * (1 - (1 - f) * saturation);
+
+                float r, g, b;
+                switch (hi)
+                {
+                    case 0:
+                        r = brightness;
+                        g = t;
+                        b = p;
+                        break;
+
+                    case 1:
+                        r = q;
+                        g = brightness;
+                        b = p;
+                        break;
+
+                    case 2:
+                        r = p;
+                        g = brightness;
+                        b = t;
+                        break;
+
+                    case 3:
+                        r = p;
+                        g = q;
+                        b = brightness;
+                        break;
+
+                    case 4:
+                        r = t;
+                        g = p;
+                        b = brightness;
+                        break;
+
+                    case 5:
+                        r = brightness;
+                        g = p;
+                        b = q;
+                        break;
+
+                    default:
+                        throw new InvalidOperationException();
+                }
+
+                return ColorHelper.FromArgb(
+                    0xff,
+                    (byte)Math.Round(r * 255d),
+                    (byte)Math.Round(g * 255d),
+                    (byte)Math.Round(b * 255d));
+            }
+        }
+    }
+
+}
---
+++ Short/SurfaceHub/ColorsAndPens/ColorsAndPensDemo/MainPage.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Input.Inking;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using ColorsAndPensDemo.TimeToShineClient.Util;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ColorsAndPensDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //inkCanvas.InkPresenter.InputDeviceTypes =
+            //     Windows.UI.Core.CoreInputDeviceTypes.Mouse |
+            //     Windows.UI.Core.CoreInputDeviceTypes.Pen;
+
+            //// Set initial ink stroke attributes.
+            //InkDrawingAttributes drawingAttributes = new InkDrawingAttributes();
+            //drawingAttributes.Color = Windows.UI.Colors.Black;
+            //drawingAttributes.IgnorePressure = false;
+            //drawingAttributes.FitToCurve = true;
+            //inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+        }
+
+        private void UIElement_OnPointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            var pos = e.GetCurrentPoint(sender as UIElement).Position;
+
+
+            var p = new Point(pos.X, pos.Y);
+            this.ChangeHue(p);
+
+            this.colorSpectrum.CapturePointer(e.Pointer);
+
+            PointerEventHandler moved = null;
+            moved = (s, args) =>
+            {
+                var posMoved = e.GetCurrentPoint(s as UIElement).Position;
+                var pMoved = new Point(posMoved.X, posMoved.Y);
+                this.ChangeHue(pMoved);
+            };
+            PointerEventHandler released = null;
+            released = (s, args) =>
+            {
+                this.colorSpectrum.ReleasePointerCapture(args.Pointer);
+                var posMoved = e.GetCurrentPoint(s as UIElement).Position;
+                var pMoved = new Point(posMoved.X, posMoved.Y);
+                this.ChangeHue(pMoved);
+                this.colorSpectrum.PointerMoved -= moved;
+                this.colorSpectrum.PointerReleased -= released;
+            };
+            this.colorSpectrum.PointerMoved += moved;
+            this.colorSpectrum.PointerReleased += released;
+        }
+
+        private void ChangeHue(Point p)
+        {
+            //  Debug.WriteLine($"X: {p.X}, Y: {p.Y}");
+
+            //var pointX = 
+
+            var py = Math.Max(0d, p.Y);
+            py = Math.Min(this.colorSpectrum.ActualHeight, py);
+
+            var px = Math.Max(0d, p.X);
+            px = Math.Min(this.colorSpectrum.ActualWidth, px);
+
+
+            var hue = (float)(py * 360f / this.colorSpectrum.ActualHeight);
+            var bright = (float)((px / 2) * 1f / this.colorSpectrum.ActualWidth) + .5f;
+
+            Debug.WriteLine($"hue: {hue}, bright: {bright}");
+
+            var h = ColorUtils.FromHsv(hue, 1f, bright);
+
+            var colorString = string.Format("#FF{0:X2}{1:X2}{2:X2}", h.R, h.G, h.B);
+
+            // Debug.WriteLine(colorString);
+
+            colorSpectrum.Background = new SolidColorBrush(h);
+
+            //var specPoint = Math.Round(py, MidpointRounding.AwayFromZero);
+
+            //this.ViewModel.ColorSpectrumPoint = Math.Round(py, MidpointRounding.AwayFromZero);
+        }
+    }
+}
